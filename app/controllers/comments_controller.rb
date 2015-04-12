bff5e0697e33ead9952776a4ecc4682610c5d45f
Chester Lam <chesterlam620@gmail.com>
2015-04-11 18:19:31 -0700

    Add destroy and destroy dependent

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,15 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comments_params)
 
-    redirect_to post_path(@post)
+    redirect_to @post
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to @post
   end
 
 
