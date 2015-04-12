4f1422bbec0b84fe9ac6d8b7f65b804c2a049399
Chester Lam <chesterlam620@gmail.com>
2015-04-12 16:03:11 -0700

    Added Factory Girl to tests

---
+++ spec/factories/posts.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :post do
+    title 'This is a valid title'
+    body 'This is a valid body'
+  end
+end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,26 +2,29 @@ require 'rails_helper'
 
 describe 'Post' do
   it "is valid with a title and a body" do
-    post = Post.new(
-      title: 'This is a blog title',
-      body: 'What a great blog.')
-    expect(post).to be_valid
+    # post = Post.new(
+    #   title: 'This is a blog title',
+    #   body: 'What a great blog.')
+    expect(FactoryGirl.build(:post)).to be_valid
   end
 
   it "is invalid without a title" do
-    post = Post.new(title: '')
+    # post = Post.new(title: '')
+    post = FactoryGirl.build(:post, title: '')
     post.valid?
     expect(post.errors[:title]).to include("can't be blank")
   end
 
   it "is invalid with a title less than 5 char" do
-    post = Post.new(title: 'a' * 4)
+    # post = Post.new(title: 'a' * 4)
+    post = FactoryGirl.build(:post, title: 'a' * 4)
     post.valid?
     expect(post.errors[:title]).to include("is too short (minimum is 5 characters)")
   end
 
   it "is invalid without a body" do
-    post = Post.new(body: '')
+    # post = Post.new(body: '')
+    post = FactoryGirl.build(:post, body: '')
     post.valid?
     expect(post.errors[:body]).to include("can't be blank")
   end
